5eabcdb1ed1d202a2542bf2e9391615a956572fe
quaaalud <dludwins@outlook.com>
2023-04-09 21:34:35 -0500

    started data agg

---
+++ __helpers__/aggregate_results.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  9 21:07:12 2023
+
+@author: dale
+"""
+
+import sys
+import pandas as pd
+from pathlib import Path
+
+
+HELPERS_DIR = Path(__file__).parent
+
+if str(HELPERS_DIR) not in sys.path:
+    sys.path.append(str(HELPERS_DIR))
+    
+
+class TwitterDataAgg:
+    
+    global HELPERS_DIR
+
+    main_dir = HELPERS_DIR.parent
+    twint_api_dir = Path(
+        main_dir, 
+        'Twint_API'
+    )
+    twint_responses_dir = Path(
+        twint_api_dir, 
+        'twint-responses'
+    )
+    
+    def __init__(self):
+        from import_and_clean_results import CleanResults
+        self.df = CleanResults().df
+        
+
+    
+if __name__ == '__main__':
+    pass
