0ecfa92b40ee10b5dff0f57e1113445795eb0273
quaaalud <dludwins@outlook.com>
2023-04-09 21:32:48 -0500

    started gui addition

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ Created on Sat Apr  8 08:47:37 2023
 @author: dale
 """
 
+import sys
+import streamlit as st
+from pathlib import Path
+
+
+MAIN_DIR = Path(__file__).parent
+
+if str(MAIN_DIR) not in sys.path:
+    sys.path.append(str(MAIN_DIR))
+    
+    
+def _get_all_results():
+    from __helpers__.aggregate_results import TwitterDataAgg
+    return TwitterDataAgg().df
+
+
+def main():
+    st.header('Twitter Data - Twint_Zero_Dashboard')
+    data = _get_all_results()
+    st.dataframe(data)
+
+
+if __name__ == '__main__':
+    main()
